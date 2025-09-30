f40c0b4f48c49d821db9fe86130c191292a74833
José Antonio Gámez Granado <jose.gamez.granado.al@iespoligonosur.org>
2025-09-30 10:13:05 +0200

    Cambios nuevos

--- Django/mysite/settings.py
+++ Django/mysite/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-gy!f&2tq$l-5c0wswgpb3%lb!0i*b9#j@08jd#gjatu-7f=lp3
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0']
+INTERNAL_IPS = ["127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'blog',
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
